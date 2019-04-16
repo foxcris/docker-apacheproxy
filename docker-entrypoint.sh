57bc14058f84bff8045043b9440fce9263842150
foxcris <361299+foxcris@users.noreply.github.com>
2019-04-16 21:26:06 +0200

    - starting apache in background - using tail to get logout

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -13,10 +13,16 @@ fi
 #List site and enable
 ls /etc/apache2/sites-available/ -1A | a2ensite *.conf
 
+#Start Apache
+/etc/init.d/apache2 start
+
+#Start Cron
+/etc/init.d/anacron start
+/etc/init.d/cron start
+
 #LETSECNRYPT
 if [ "$LETSENCRYPTDOMAINS" != "" ]
 then
-  /usr/sbin/apache2ctl start
   domains=$(echo $LETSENCRYPTDOMAINS | tr "," "\n")
   for domain in $domains
   do
@@ -27,12 +33,6 @@ then
     fi
     certbot --apache --noninteractive --expand --domains $domain --agree-tos --email $LETSENCRYPTEMAIL
   done
-  /usr/sbin/apache2ctl stop
 fi
 
-#Start Cron
-/etc/init.d/anacron start
-/etc/init.d/cron start
-
-#Launch Apache on Foreground
-/usr/sbin/apache2ctl -D FOREGROUND
+tail -f /var/log/apache2/access.log -f /var/log/apache2/error.log
